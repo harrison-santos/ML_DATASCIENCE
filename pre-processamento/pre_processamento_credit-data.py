e1d9c88f6c9eb2c2d96febc11edc3ff3a3f275bc
harrison-santos <harrisonspeed1997@gmail.com>
2018-12-10 11:41:17 -0300

    Feito pre-processamento da base credit-data.csv.

---
+++ pre-processamento/pre_processamento_credit-data.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+#1-carregamento de dados. 2-Tratamento de valores inconsistentes. 3-Tratamento de valores faltantes. 4- Escalonamento de atributos. 5-Transformação de variáveis categóricas 
+import pandas as pd
+pd.set_option("display.max_columns", 8)
+base = pd.read_csv('bases/credit-data.csv')
+base.describe()
+
+
+#1) valores inconsistentes. Valores negativos em idade. 
+#Possíveis Passos: Apagar os registros com problemas, preencher manualmente, preencher valores com a média/mediana/most_frequent.
+base.loc[base['age'] < 0]
+base.mean()#faz a média com valores negativos
+age_mean = base['age'][base.age > 0].mean()
+base.loc[base.age < 0, 'age'] = age_mean 
+base.loc[base['age'] < 0]#valor corrigido
+
+#2) valores faltantes
+pd.isnull(base['age'])
+base.loc[pd.isnull(base['age'])]
+
+previsores = base.iloc[:, 1:4].values#todas as linhas, e colunas do index 1 até 3
+classe = base.iloc[:, 4]#todas as linhas, e a coluna no index 4
+
+
+#tratamento de valores null com sklearn
+from sklearn.preprocessing import Imputer#O imputer colocará um valor de entrada.
+imputer = Imputer(missing_values='NaN', strategy='mean', axis=0)#ctrl + i para informações.
+imputer = imputer.fit(previsores[:, 0:3])
+previsores[:, 0:3] = imputer.transform(previsores[:, 0:3])
+
+#Escalonamento de Atributos. Temos duas possíveis formas de escalonar: padronização e normalização. Com isso os algoritmos não farão cálculos matemáticos com números muito grandes.
+#Padronização: x = x - média(x)/desvio padrão(x). Normalização: x = x - mínimo(x)/máximo(x)-mínimo(x)
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+previsores = scaler.fit_transform(previsores)
+
+
